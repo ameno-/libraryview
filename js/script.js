36ba9d341e295baa89d2b464ee5878df1535b0e2
Ameno-work <ameno.osman13@gmail.com>
2013-12-11 16:39:36 -0500

    lots of work
    basically rebuilt the layout and made the js smarter

--- js/script.js
+++ js/script.js
@@ -9,10 +9,42 @@ var infoView = $('#moreInfo').html(),
 	infoData;
 
 $('#info').on('click', function () {
-	infoData = JSON.parse('{"heading":"More Info","title":"Title","titleVal":"Compendium of Pharmaceuticals and Specialties 2014 (CPS)","thumb":"img/CPhA_Book_Image.png", "author":"Author","authorVal":"Editor-in-Chief Carol Repchinsky, BSP","publisher":"Publisher","publisherVal":"Canadian Pharmacists Association" ,"date":"11/04/2020","isbn":"978-1-894402-70-5","subj":"Subject","subjVal":"","orgPub":"Originial Publication","language":"Language","languageVal":"English","desc":"Description","descVal":"The Compendium of Pharmaceuticals and Specialties (CPS) is Canada\'s source for drug information. It contains more than 2000 product monographs for drugs, vaccines and natural health products, including 200 new products for the Canadian market. This definitive resource also has 151 monographs written by the editorial staff of CPhA, based on the best available evidence and reviewed by expert physicians and pharmacists. Clinical tools, product images and directories of sources for drug and health care information are included. A comprehensive cross-referenced index of generic and brand names ensures you can easily find what you\'re looking for. The product monographs are prepared by the pharmaceutical manufacturers and approved by Health Canada."}');
+	infoData = JSON.parse('{"heading":"More Info","title":"Title","titleVal":"Compendium of Pharmaceuticals and Specialties 2014 (CPS)","thumb":"img/CPhA_Book_Image.png", "aboutBook":"about.html","aboutUs":"about.html","credits":"credits.html","author":"Author","authorVal":"Editor-in-Chief Carol Repchinsky, BSP","publisher":"Publisher","publisherVal":"Canadian Pharmacists Association" ,"date":"11/04/2020","isbn":"978-1-894402-70-5","subj":"Subject","subjVal":"","orgPub":"Originial Publication","language":"Language","languageVal":"English","desc":"Description","descVal":"The Compendium of Pharmaceuticals and Specialties (CPS) is Canada\'s source for drug information. It contains more than 2000 product monographs for drugs, vaccines and natural health products, including 200 new products for the Canadian market. This definitive resource also has 151 monographs written by the editorial staff of CPhA, based on the best available evidence and reviewed by expert physicians and pharmacists. Clinical tools, product images and directories of sources for drug and health care information are included. A comprehensive cross-referenced index of generic and brand names ensures you can easily find what you\'re looking for. The product monographs are prepared by the pharmaceutical manufacturers and approved by Health Canada."}');
 	var	infoOutput = Mustache.render(infoView, infoData);
-	$('body').append("<div class='overlay'></div>");
-	$('#infoModal').removeClass('hide').append(infoOutput);
+	$('.overlay').removeClass('hide');
+	$('#infoModal').append(infoOutput);
+
+	$('#infoModal').removeClass('hide')
+		$('.overlay').on('click', function(){
+			$('#infoModal').addClass('hide');
+			$('.overlay').addClass('hide');
+		});
+
+	$('.aboutLinks').children().on('click', function (ev) {
+			$('.infoList').addClass('hide');
+			$('.iframeCont').show('fast');
+		$('#close').on('click', function () {
+			$('.infoList').removeClass('hide');
+			$('.iframeCont').hide('fast');
+		});
+	});
+});
+
+var mql = window.matchMedia("(orientation: portrait)");
+if(mql.matches) {  
+	$('.tiles').addClass("small-block-grid-2");
+} else {  
+	$('.tiles').addClass("small-block-grid-3");
+}
+mql.addListener(function(m) {
+	if(m.matches) {
+		$('.tiles').removeClass("small-block-grid-3");
+		$('.tiles').addClass("small-block-grid-2")
+	}
+	else {
+		$('.tiles').removeClass("small-block-grid-2");
+		$('.tiles').addClass("small-block-grid-3")
+	}
 });
 //check if book is downloaded and update icons
 if (libraryData.isDownloaded) {
@@ -22,3 +54,4 @@ if (libraryData.isDownloaded) {
 	$('#accessBook').addClass("fa fa-cloud-download fa-2x");
 };
 };
+

305a8c3cc70fc7e571ef3d1cf29faa8a666e61a5
Ameno-work <ameno.osman13@gmail.com>
2013-12-28 12:43:40 -0500

    work

--- js/script.js
+++ js/script.js
@@ -1,24 +1,24 @@
+//ensure icon changes only apply to active tile (get parent element and apply changes in context)
 window.onload = function () {
 FastClick.attach(document.body);
-$('#accessBook').css('font-size','2.2em');
+$('.accessBook').css('font-size','2.2em');
 var downloadIcon = "icon-download2",
 			trashIcon = "icon-trash2",
-			bookIcon = "icon-book"
+			bookIcon = "icon-book";
 //build library view and data
 var libraryData = JSON.parse('{"bookTitle":"Compendium of Pharmaceuticals and Specialties 2014 (CPS) English", "imgurl":"../img/Tile_Background.png","author":"Editor-in-Chief Carol Repchinsky, BSP","otherTitle":"Other Title","isDownloaded":false ,"date":"11/04/2020","thumb":"img/CPhA_Book_Image.png"}');
 var libraryView = $("#lib").html();
 var libraryOutput = Mustache.render(libraryView, libraryData);
 $(document.body).append(libraryOutput);
 var backgroundImage = libraryData.imgurl;
-$('.addImg').css('background-image', 'url(../img/Tile_Background.png)');
+
 $('.book').addClass('addImg');
 var infoView = $('#moreInfo').html(),
 	infoData;
 
 //render and display more info modal on click (destroys itself when window is exited)
-$('#info').on('click', function () {
+$('.info').on('click', function () {
 	infoData = JSON.parse('{"heading":"More information","title":"Title","titleVal":"Compendium of Pharmaceuticals and Specialties 2014 (CPS)","thumb":"img/CPhA_Book_Image.png", "aboutBook":"about.html","aboutUs":"about.html","credits":"credits.html","author":"Author","authorVal":"Editor-in-Chief Carol Repchinsky, BSP","publisher":"Publisher","publisherVal":"Canadian Pharmacists Association" ,"date":"11/04/2020","isbn":"978-1-894402-70-5","subj":"Subject","subjVal":"","orgPub":"Originial Publication","language":"Language","languageVal":"English","desc":"Description","descVal":"The Compendium of Pharmaceuticals and Specialties (CPS) is Canada\'s source for drug information. It contains more than 2000 product monographs for drugs, vaccines and natural health products, including 200 new products for the Canadian market. This definitive resource also has 151 monographs written by the editorial staff of CPhA, based on the best available evidence and reviewed by expert physicians and pharmacists. Clinical tools, product images and directories of sources for drug and health care information are included. A comprehensive cross-referenced index of generic and brand names ensures you can easily find what you\'re looking for. The product monographs are prepared by the pharmaceutical manufacturers and approved by Health Canada."}');
-	
 	var	infoOutput = Mustache.render(infoView, infoData);
 		
 	//show grey background, append rendred more info view to modal window, show modal window
@@ -27,6 +27,12 @@ $('#info').on('click', function () {
 	$('#infoModal').removeClass('hide');
 	//dynamically remove list items with empty values
 	removeEmpty();
+
+	//click handler for the modal window
+	$('#closeModal').on('click', function(){
+			$('#infoModal').addClass('hide').children().remove();
+			$('.overlay').addClass('hide');
+		});
 	//controlll show/hide of elements for the mroe info iframes
 	$('.aboutLinks').children().on('click', function (ev) {
 			$('.infoList,.stickyLeft').addClass('hide');
@@ -38,10 +44,7 @@ $('#info').on('click', function () {
 			$('.iframeCont').hide('fast');		
 		});
 	});
-$('#closeModal').on('click', function(){
-			$('#infoModal').addClass('hide').children().remove();
-			$('.overlay').addClass('hide');
-		});
+		//handler for closing modal window via click outside of window
 		$('.overlay').on('click', function(){
 			$('#infoModal').addClass('hide').children().remove();
 			$('.overlay').addClass('hide');
@@ -58,39 +61,48 @@ mql.addListener(function(m) {
 	if(m.matches) {
 		$('.tiles').removeClass("small-block-grid-3");
 		$('.tiles').addClass("small-block-grid-2");
-		console.log("portrait");
-	}
-	else {
+	}else{
 		$('.tiles').removeClass("small-block-grid-2");
 		$('.tiles').addClass("small-block-grid-3");
-		console.log("landscape");
 	}
 });
 
 //check if book is downloaded and update icons
 if (libraryData.isDownloaded) {
 	//show book icon and garbage icon
-	$('#garbage').addClass(trashIcon);
-	$('#accessBook').addClass(bookIcon);
+	$('.garbage').addClass(trashIcon);
+	$('.accessBook').addClass(bookIcon);
 }else{
 	//show download icon
-	$('#accessBook').addClass(downloadIcon);
+	$('.accessBook').addClass(downloadIcon);
 };
 
 //progress bar
-	$('#accessBook').on('click', function () {
+	$('.accessBook').on('click', function () {
 		//only excecute download if the icon class = donwloaded --> (data.isDownloaded = false)
-	if ($('#accessBook').hasClass(downloadIcon)) {
-				progress();
-				
-				}
+		if ($('.accessBook').hasClass(downloadIcon)) {
 
-			//hide
-			$('#accessBook').removeClass(downloadIcon);
-			$('#garbage').addClass(trashIcon);
-			$('#accessBook').addClass(bookIcon);
-			$('progress').addClass('hide');
-			$('.grayed').addClass('hide');
+				$('progress').removeClass('hide');
+				$('.grayed').removeClass('hide');
+				var i = 0;
+
+					do {
+						i +=5;
+						$('progress').attr('value',i);
+						  setInterval(function() {					      
+					      	
+						}, 1000);
+					} while (i < $('progress').attr('max'));
+					//hide
+					$('.accessBook').removeClass(downloadIcon);
+					$('.garbage').addClass(trashIcon);
+					$('.accessBook').addClass(bookIcon);
+					$('progress').addClass('hide');
+					$('.grayed').addClass('hide');
+					
+				};
+
+			
 		});
 };
 
@@ -102,15 +114,3 @@ if (libraryData.isDownloaded) {
 	    	}
 		});
 	};
-
-	function progress () {
-				$('progress').removeClass('hide');
-				$('.grayed').removeClass('hide');
-					var i = 0;
-					if (i <= $('progress').attr('max')){
-					setInterval(function() {
-					      i +=5;
-					      $('progress').attr('value',i);		
-					}, 1000);
-				}
-			}
